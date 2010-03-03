4cf414106884e4b181d674a6f2c1520abf5403b7
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-03-03 16:19:10 -0600

    Update to new bit array parser

--- common/bitstream.py
+++ common/bitstream.py
@@ -1,150 +1,76 @@
-# Based on BitBuffer
+import array
 
 class BitStream(object):
-    def __init__(self, buf=''):
-        self.buf = [ord(x) for x in buf]
+    _bytesperlong = 32/len(array.array('L',32*' '))
+    _bitsperlong = _bytesperlong*8
 
-        self.pos = 0
-        self.len = len(buf)*8
+    def __init__(self, source_str):
+        self._totalbits = len(source_str) * 8
+        self._position = 0
 
-        self.closed = False
-        self.softspace = 0
+        # Pad to longword boundary, then make an array
 
-    def close(self):
-        if not self.closed:
-            self.closed = True
-            del self.buf, self.pos, self.len, self.softspace
+        source_str += -len(source_str) % self._bytesperlong * chr(0)
+        self._bitstream = array.array('L', source_str)
 
-    def isatty(self):
-        if self.closed:
-            raise ValueError, "I/O operation on closed file"
-        return 0
-
-    def seekbits(self, pos, mode=0):
-        if self.closed:
-            raise ValueError, "I/O operation on closed file"
-        if mode == 1:
-            pos += self.pos
-        elif mode == 2:
-            pos += self.len
-        self.pos = max(0, pos)
-    
-    def seek(self, pos, mode=0):
-        return self.seekbits(pos*8, mode)
+    def seek(self, offset, whence=0):
+        self._position = offset + (0, self._position, self._totalbits)[whence]
 
     def tell(self):
-        if self.closed:
-            raise ValueError, "I/O operation on closed file"
-        return self.pos
-
-    def flush(self):
-        if self.closed:
-            raise ValueError, "I/O operation on closed file"
-
-    def truncate(self, size=None):
-        if self.closed:
-            raise ValueError, "I/O operation on closed file"
-        if size is None:
-            size = self.pos
-        elif size < 0:
-            raise IOError(EINVAL, "Negative size not allowed")
-        elif size < self.pos:
-            self.pos = size
-
-        self.len = size
-        self.buf = self.buf[:(size//8)+(size%8 != 0)]
-        if self.buf != []:
-            self.buf[-1] = self.buf[-1] & (1<<(size%8))-1
-        
-    def writebits(self, n, bitlen):
-        """Writes bits"""
-        
-        if self.closed:
-            raise ValueError, "I/O operation on closed file"
-
-        n &= (1L << bitlen)-1
-
-        newpos = self.pos + bitlen
-
-        startBPos = self.pos%8
-        startBlock = self.pos//8
-
-        endBPos = newpos%8
-        endBlock = newpos//8+(endBPos != 0)
-
-        while len(self.buf) < endBlock: self.buf += [0]
-
-        pos = startBPos
-
-        while bitlen > 0:
-            bitsLeft=8-(pos%8)
-            if bitsLeft > bitlen: bitsLeft=bitlen
-            
-            mask=(1<<bitsLeft)-1
-
-            self.buf[startBlock+(pos//8)] ^= self.buf[startBlock+(pos//8)]&(mask<<(pos%8))
-            self.buf[startBlock+(pos//8)] |= int(n&mask)<<(pos%8)
-        
-            n >>= bitsLeft
-            bitlen -= bitsLeft
-            
-            pos+=bitsLeft
-
-        self.pos = newpos
-        if self.pos > self.len:
-            self.len = self.pos
+        return self._position
 
     def align(self):
-        """Aligns to the next byte to the right"""
-        self.seekbits(8 - (self.tell() % 8), 1)
+        address, offset = divmod(self._position, 8)
+        if offset:
+            self.seek(8 - offset, 1)
 
-    def readbits(self, bitlen):
-        """Reads bits"""
-        
-        if self.closed:
-            raise ValueError, "I/O operation on closed file"
-
-        newpos = self.pos + bitlen
-
-        startBPos = self.pos%8
-        startBlock = self.pos//8
-
-        endBPos = newpos%8
-        endBlock = newpos//8+(endBPos != 0)
-
-        ret=0
-
-        pos = startBPos
-        shift = 0
-
-        while bitlen > 0:
-            bitsLeft=8-(pos%8)
-            if bitsLeft > bitlen: bitsLeft=bitlen
-
-            mask = (1<<bitsLeft)-1
-
-            ret |= long((self.buf[startBlock+(pos//8)] >> (pos%8)) & mask) << shift
-
-            shift += bitsLeft
-
-            bitlen -= bitsLeft
-            pos += bitsLeft
-
-        self.pos = newpos
-        return ret
-
-    def getvalue(self):
-        """Get the buffer"""
-        
-        return ''.join(map(chr, self.buf))
-
-    def write(self, s):
-        for c in str(s):
-            self.writebits(ord(c), 8)
-
-    def read(self, i):
+    def read(self, length):
         self.align()
-        ret = []
-        for i in range(i):
-            ret.append(chr(self.readbits(8)))
-        return ''.join(ret)
+
+        longaddress = self._position / self._bitsperlong
+        endaddress = (self._position + length) / self._bitsperlong
+
+        outval = self._bitstream[longaddress:endaddress]
+
+        self._position += length * 8
+
+        return outval
+
+    def readbits(self, length):
+        bitsperlong, position = self._bitsperlong, self._position
+        
+        if position < 0 or position + length > self._totalbits:
+            raise IndexError, "Invalid bitarray._position/length"
+
+        longaddress, bitoffset = divmod(position, bitsperlong)
+
+        # We may read bits in the final word after ones we care
+        # about, so create a mask to remove them later.
+
+        finalmask = (1L << length) - 1
+
+        # We may read bits in the first word before the ones we
+        # care about, so bump the total bits to read by this
+        # amount, so we read enough higher-order bits.
+
+        length += bitoffset
+
+        # Read and concatenate every long which contains a bit we need
+
+        outval,outshift = 0L,0
+        while length > 0:
+            outval += self._bitstream[longaddress] << outshift
+            longaddress += 1
+            outshift += bitsperlong
+            length -= bitsperlong
+
+        # length is now basically a negative number which tells us
+        # how many bits to back up from our current position.
+
+        self._position = longaddress*bitsperlong + length
+
+        # Shift right to strip off the low-order bits we
+        # don't want, then 'and' with the mask to strip
+        # off the high-order bits we don't want.
+
+        return (outval >> bitoffset) & finalmask
